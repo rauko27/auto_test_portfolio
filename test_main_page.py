8c15cf73dc3caf60a801f9bd7e1e06d1fc6213eb
rauko27 <rauko27@gmail.com>
2019-07-20 23:00:39 +0300

    some update

--- test_main_page.py
+++ test_main_page.py
@@ -1,18 +1,18 @@
 from .main_page import MainPage
 from .login_page import LoginPage
+from .locators import MainPageLocators
 
 def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
+    link = MainPageLocators.MAIN_LINK
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
 
 
 def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209?promo=midsummer "
+    link = MainPageLocators.MAIN_LINK
     page = MainPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес
     page.open()                      # открываем страницу
     page.go_to_login_page()          # выполняем метод страницы - переходим на страницу логина
     login_page = LoginPage(browser, link)
-    # login_page.open()
     login_page.should_be_login_page()
