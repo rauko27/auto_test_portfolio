8f2f187e2df99b6305bf6ca1f78223748a7a22ba
rauko27 <rauko27@gmail.com>
2019-07-21 14:43:34 +0300

    change link for tests

--- test_product_page.py
+++ test_product_page.py
@@ -1,12 +1,25 @@
 from .Pages.locators import MainPageLocators
 from .Pages.product_page import ProductPage
 import time
+import pytest
 
-def test_guest_can_add_product_to_cart(browser):
-    link = 'http://selenium1py.pythonanywhere.com/ru/catalogue/the-shellcoders-handbook_209/?promo=newYear'
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer3",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer4",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer5",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer6",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer7",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer8",
+                                  "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer9"])
+def test_guest_can_add_product_to_cart(browser, link):
+    link = link.format(link)
     prod_page = ProductPage(browser, link)
     prod_page.open()
     prod_page.add_to_cart()
+    time.sleep(2)
     prod_page.solve_quiz_and_get_code()
     prod_page.shoud_be_right_name_in_cart()
     prod_page.shoud_be_right_price_in_cart()
